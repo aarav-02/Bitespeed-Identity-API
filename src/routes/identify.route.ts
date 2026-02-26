6728569b9db910e1a111604224e6a6110e585e45
Prince Sahu <princesahu0008@gmail.com>
2026-02-27 01:13:11 +0530

    .

--- src/routes/identify.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { identifyContact } from "../controllers/identify.controller";
-
-const router = Router();
-
-router.post("/", identifyContact);
-
-export default router;
